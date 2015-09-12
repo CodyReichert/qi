0fb6e701aa059df00c2a277906a3f7328c43d6f1
Cody Reichert <cody@reichertbrothers.com>
2015-09-12 16:03:16 -0500

    add utility function for finding asdf system's path

--- src/util.lisp
+++ src/util.lisp
@@ -1,15 +1,17 @@
 (in-package :cl-user)
 (defpackage qi.util
   (:use :cl)
-  (:export :sym->str
+  (:export :asdf-system
            :is-tar-url?
            :is-git-url?
-           :is-gh-url?))
+           :is-gh-url?
+           :sym->str))
 (in-package :qi.util)
 
 ;; Code:
 
 (defun sym->str (sym)
+  ":<sym> -> \"sym\"."
   (string-downcase (symbol-name sym)))
 
 (defun is-tar-url? (str)
@@ -25,3 +27,10 @@
 (defun is-gh-url? (str)
   "Is <str> a github url."
   (ppcre:scan "^https?//github.*" str))
+
+
+(defun asdf-system-path (sys)
+  "Find the pathname for a system, return NIL if it's not available."
+  (handler-case
+      (asdf:component-pathname (asdf:find-system sys))
+    (error () () nil)))

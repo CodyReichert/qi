1115cfb789db36df6e0e20e0e783ae80fa43c858
Alex Dunn <dunn.alex@gmail.com>
2016-10-29 08:59:32 -0700

    make is-git-url? more strict

--- t/util_test.lisp
+++ t/util_test.lisp
@@ -1,16 +1,18 @@
 (in-package :qi-test)
 
-(plan 5)
+(plan 9)
 
 (ok (qi.util::is-tar-url? "https://github.com/CodyReichert/qi/master/master.tar.gz") "Is a tar url.")
 (ok (qi.util::is-tar-url? "https://github.com/CodyReichert/qi/master/master.tgz") "Is a tar url.")
 (ok (not (qi.util::is-tar-url? "https://github.com/CodyReichert/qi/master/master.bz2")) "Is not tar url.")
 
-(ok (qi.util::is-git-url? "https://github.com/CodyReichert/qi") "Is a git url.")
 (ok (qi.util::is-git-url? "https://github.com/CodyReichert/qi.git") "Is a git url.")
+(ok (not (qi.util::is-git-url? "https://github.com/CodyReichert/qi")) "Is not a git url.")
 
-(ok (qi.util::is-gh-url? "https://github.com/CodyReichert/qi") "Is a github url.")
+(ok (qi.util::is-gh-url? "https://github.com/CodyReichert/qi") "Is a GitHub url.")
+(ok (not (qi.util::is-gh-url? "https://gitlab.com/CodyReichert/qi.git")) "Is not a GitHub url.")
 
 (ok (qi.util::is-hg-url? "https://bitbucket.org/tarballs_are_good/map-set.hg") "Is a mercurial url.")
+(ok (not (qi.util::is-hg-url? "https://bitbucket.org/tarballs_are_good/map-set")) "Is not a mercurial url.")
 
 (finalize)

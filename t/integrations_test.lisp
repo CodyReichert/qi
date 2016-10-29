ce5a40b78a16591cb440e0fe215c213c04479561
Alex Dunn <dunn.alex@gmail.com>
2016-10-29 09:06:15 -0700

    [test] copy fixture instead of moving it

--- t/integrations_test.lisp
+++ t/integrations_test.lisp
@@ -10,13 +10,21 @@
                                 :location (qi.packages::http "https://github.com/tokenrove/anaphora/tarball/master")
                                 :src-path (merge-pathnames tar-dir "anaphora-master.tar.gz")
                                 :sys-path (merge-pathnames tar-dir "anaphora-latest")
-                                :version "latest")))
+                                :version "latest"))
+      (tmpfile (merge-pathnames "anaphora-latest.tar.gz" (qi.paths:+dep-cache+))))
   (qi::bootstrap (qi.packages::dependency-name dep))
   (ensure-directories-exist (qi.paths:+dep-cache+))
-  (rename-file (merge-pathnames "anaphora-master.tar.gz" tar-dir)
-               (merge-pathnames "anaphora-latest.tar.gz" (qi.paths:+dep-cache+)))
+
+  ;; Copy the fixture to TMPDIR
+  (unless (probe-file tmpfile)
+    (with-open-file (source (merge-pathnames "anaphora-master.tar.gz" tar-dir)
+                            :direction :input
+                            :element-type '(unsigned-byte 8))
+      (with-open-file (target tmpfile :direction :output :element-type '(unsigned-byte 8))
+        (uiop:copy-stream-to-stream source target :element-type '(unsigned-byte 8)))))
+
   (let ((out (qi.packages::unpack-tar dep)))
-    (ok (not (eql out (dependency-sys-path dep))) "Extracts tarball with different name.")
-    (ok (probe-file (dependency-sys-path dep)) "Extracts tarball and exists")))
+    (ok (not (eql out (qi.packages:dependency-sys-path dep))) "Extracts tarball with different name.")
+    (ok (probe-file (qi.packages:dependency-sys-path dep)) "Extracts tarball and exists")))
 
 (finalize)

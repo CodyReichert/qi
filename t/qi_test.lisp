6c18fc82825bf1e86fa923de759109ee4ae7490f
Alex Dunn <dunn.alex@gmail.com>
2017-05-28 08:13:45 -0700

    qi_test: reset variables between tests

--- t/qi_test.lisp
+++ t/qi_test.lisp
@@ -5,16 +5,26 @@
         :prove))
 (in-package :qi-test-packages)
 
+(defun reset-metadata ()
+  "Unset the variables from `qi:bootstrap'.  We run this between
+install tests to ensure a clean environment, simulating how they're
+run from the command-line."
+  (setf qi::+project-name+ nil)
+  (setf qi.manifest::+manifest-packages+ nil))
+
 (plan 5)
 
 (ok (qi:hello))
 
 (ok (qi:install-global :yason))
+(reset-metadata)
 
 (load "t/resources/project/test-project.asd")
 (ok (qi:install :test-project))
+(reset-metadata)
 
 (ok (qi:install-from-qi-file "t/resources/project/qi.yaml"))
 (is-error (qi:install-from-qi-file "/path/to/nonexistent/qi.yaml") 'error)
+(reset-metadata)
 
 (finalize)
